0d581ddffb37a0ab6928eaab8d1df9104b2a49a4
davit <david.ortiz.developer@gmail.com>
2022-10-22 20:23:14 -0500

    first commit

---
+++ PagaTapp/PagaTapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PagaTapp
+//
+//  Created by edgar david ortiz diaz on 22/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
