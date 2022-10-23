8c8df808e1d2ab042e6aec8fc7f2ac13756a1f0f
davit <david.ortiz.developer@gmail.com>
2022-10-22 22:14:57 -0500

    Folders created, project organized

---
+++ PagaTapp/PagaTapp/Entities/BankObject.swift
@@ -0,0 +1,14 @@
+//
+//  BankObject.swift
+//  PagaTapp
+//
+//  Created by edgar david ortiz diaz on 22/10/22.
+//
+
+import Foundation
+struct BankObject {
+    var bankName: String
+    var description: String
+    var age: Int
+    var url: String
+}
---
+++ PagaTapp/PagaTapp/Modules/BanksList/BanksListPresenter.swift
@@ -0,0 +1,18 @@
+//
+//  BanksListPresenter.swift
+//  PagaTapp
+//
+//  Created by edgar david ortiz diaz on 22/10/22.
+//
+
+import Foundation
+protocol BanksListPresenterProtocol {
+    var view: BanksListViewControllerProtocol? {get set}
+    var router: BanksListRouterProtocol? {get set}
+    var interactor: BanksListInteractorProtocol? {get set}
+}
+class BanksListPresenter: BanksListPresenterProtocol {
+    var interactor: BanksListInteractorProtocol?
+    var router: BanksListRouterProtocol?
+    var view: BanksListViewControllerProtocol?
+}
